0d8d7fc47310bab3169b0aa8c55a81b05af646d4
HenryD-Dev <henry.doan129@gmail.com>
2022-10-16 20:48:33 -0500

    Updated BBFUN file

---
+++ BBFUN.py
@@ -0,0 +1,10 @@
+# Author: Henry Doan
+# Project 5 Part 1
+# Function file to caculate batting average and
+# slugging percentage.
+
+def battingAvg(num1,num2,num3,num4):
+    return (num1 + num2 + num3 + num4)
+
+def sluggingPercent(num1,num2,num3,num4):
+    return (num1 + num2 * 2 + num3 * 3 + num4 * 4)
