5477a1bee26154bf9dfced22cac9d24b6b9124ee
HenryD-Dev <henry.doan129@gmail.com>
2022-10-29 23:48:51 -0500

    Added user input for project 7

--- Henry_Doan_Project7.py
+++ Henry_Doan_Project7.py
@@ -1,8 +1,21 @@
 # Author: Henry Doan
 # Project 7
-# project will use string manipulation to create and solve a realistic problem,
-# and apply the knowlege of all the previous projects.
+# project will use string manipulation to create and validate
+# a school survey on the school overall experience from the user.
+
+# main function
+
 
 def main():
+    # User Inputs
+    question1 = input("From 0 to 10 how would you rate the cleanliness of your school:\n")
+    isDigit = question1.isdigit()
+    print(question1)
+    print(isDigit)
+    while isDigit == False:
+        question1 = input("Re-Enter your answer using numbers:\n")
+        isDigit = question1.isdigit()
+    print(question1)
+    print(isDigit)
     
 main()
