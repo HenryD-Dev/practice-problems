4031e6aefb71b7d7260c56438b84e0c13ee26a81
HenryD-Dev <henry.doan129@gmail.com>
2022-11-09 16:59:10 -0600

    Updated the name and description of the project.

--- Henry_Doan_Project8.py
+++
@@ -1,2 +0,0 @@
-# Author: Henry Doan
-# Project 8
---
+++ Henry_Doan_Project8_Part2.py
@@ -0,0 +1,11 @@
+# Author: Henry Doan
+# Project 8
+# program can read the contents of a file, 
+# where each record in the file has 5 fields,
+# and store all of the records in a single data structure.
+# The program should then present a menu of options to the user.
+# The user should be able to select one of the following three actions to take:
+#1) Display the average number of hours earned.
+#2) Display the average GPA.
+#3) Display the major of a student with a specified Last name.
+
