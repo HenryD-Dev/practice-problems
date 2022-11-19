f6162ff75ed0fb483cfb23a0a31db09afda46c6c
HenryD-Dev <henry.doan129@gmail.com>
2022-11-19 16:27:38 -0600

    Finished Total Sales file

--- Henry_Doan_Total_Sales.py
+++ Henry_Doan_Total_Sales.py
@@ -4,6 +4,21 @@
 # The amounts should be stored in a list.
 # Use a loop to calculate the total sales for the week and display the result.
 
-def main():
-    saleDays = int(input("Enter the store sales for each day of the week"))
-    
+weekTotal = []
+
+# Input validation for input less than zero
+def lessZero(x):
+    while x < 0:
+        x = float(input("\nRe-Enter a number greater than 0:\n"))
+        weekTotal.append(x)
+
+# The store sales for each day in the week
+def weekSale():
+    for saleDays in range(7):
+        saleDays = float(input("\nEnter the store sales for the day:\n"))
+        if saleDays > 0:
+            weekTotal.append(saleDays)
+        else: lessZero(saleDays)
+weekSale()
+
+print(f"The total sales for the week is: {sum(weekTotal)}$")
