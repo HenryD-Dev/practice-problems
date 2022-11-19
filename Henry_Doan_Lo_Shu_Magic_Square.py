c1ccd9d72e38aabcd5eb62b62c603d202d887db3
HenryD-Dev <henry.doan129@gmail.com>
2022-11-18 22:50:29 -0600

    Project 8 part 1 updated description

--- Henry_Doan_Lo_Shu_Magic_Square.py
+++ Henry_Doan_Lo_Shu_Magic_Square.py
@@ -1,2 +1,6 @@
 # Author: Henry Doan
 # Project 8 part 1
+# In a program you can simulate a magic square using a two-dimensional list.
+# Write a function that accepts a two-dimensional list as an argument
+# and determines whether the list is a Lo Shu Magic Square.
+# Test the function in a program.
