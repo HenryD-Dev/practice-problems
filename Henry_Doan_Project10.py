87a02e8e4ec08de1128e900730dba915d8c48ad4
HenryD-Dev <henry.doan129@gmail.com>
2022-11-28 20:23:40 -0600

    Added description for Project 10

---
+++ Henry_Doan_Project10.py
@@ -0,0 +1,17 @@
+# Author: Henry Doan
+# Project 10
+
+# In this project we are going to calculate several values for each player. 
+# One is the aforementioned batting average.
+# A second calculated value is called the slugging percentage.
+# This value is found as follows: (1*Singles + 2*Doubles + 3*Triples + 4*HomeRuns)/AB.
+# Our data does not provide the number of singles directly but it does tell us the number of
+# hits from which we can subtract the number of doubles,
+# triples, and home runs to get to the number of singles.
+# Additionally, you will need to calculate the overall batting average for all 25
+# players combined. Note that this is different than the mean batting average of the 25 players.
+# After all calculations are performed,
+# create a nicely formatted report (and send it to an output file)
+# that shows the top five players for each of the 6 individually calculated values.
+# Additionally, include in this report the overall batting average for all 25 players combined.
+
